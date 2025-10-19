4ca7d2d6dcb70b8bbc5daa541e1818762b2df3f2
akamachowa-cyber <akamachowa@gmail.com>
2025-10-19 22:43:49 +0400

    Update test_library.py

--- test_library.py
+++ test_library.py
@@ -1,7 +1,12 @@
+# test_library.py
+
+import pytest
 from book import EBook, PrintedBook
 
-def test_calculate_late_fee():
+def test_calculate_late_fee_ebook():
     ebook = EBook("EBook Test", "Alice", 10)
-    printed_book = PrintedBook("Printed Test", "Bob", 25)
     assert ebook.calculate_late_fee(3) == 0
+
+def test_calculate_late_fee_printed_book():
+    printed_book = PrintedBook("Printed Test", "Bob", 25)
     assert printed_book.calculate_late_fee(4) == 2.0
